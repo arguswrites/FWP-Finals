84e25ddd3162097cc5825ccf99f620fd5ec7f7d6
arguswrites <93747279+arguswrites@users.noreply.github.com>
2022-06-09 10:47:53 +0800

    Add files via upload

--- Portfolio/res/main.js
+++ Portfolio/res/main.js
@@ -1,28 +1,53 @@
+const colors = [
+    [//light group
+        "#21274b",//font
+        "#c9d2df"//bg
+    ], 
+    [ //dark group
+        "#ded8b4",//font
+        "#131415"//bg
+    ],
+    [//others
+        "#ffb185", //nav
+        "#aaa"//
+    ]
+];
+
 function dmode(){
     let body = document.querySelector('body');
     let nav = document.getElementsByClassName('nav');
 
-    body.style.backgroundColor = rgba(24,26,26,1);
-    nav.style.color = white;
+    function dm(){
+        body.style.backgroundColor = colors[1][1];
+        nav.style.color = colors[1][0];
+        console.log('Changed to dark mode');
+    }
+    function lm(){
+        body.style.backgroundColor = colors[0][1];
+        nav.style.color = colors[0][0];
+        console.log('Changed to light mode');
+    }
+
+    if(nav.style.color = colors[0][0]){
+        lm();
+    } else{
+        dm();
+    }
+
 }
 
 
 function nav(){
     let opt = document.getElementById('opt');
     let innav = document.getElementById('nav');
-    let outnav = document.getElementsByClassName('nav');
-    let panel = document.getElementsByClassName('panel_board'); 
 
     function openNav(){
-        outnav.style.backgroundColor = "#0000ff";
-
         opt.style.flex = '1';
         opt.style.marginRight = '2.5%';
         opt.style.width == '7%';
     
         innav.style.display = "flex";
 
-        //panel.style.filter = "brightness(50%)";
         console.log('open');
     }
 
