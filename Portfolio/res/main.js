06f96a8c14818d1b56ca7d82d5498a982b4411b2
arguswrites <93747279+arguswrites@users.noreply.github.com>
2022-06-17 10:09:16 +0800

    Add files via upload
    Added pages and css changes. Functionality updates

--- Portfolio/res/main.js
+++ Portfolio/res/main.js
@@ -1,28 +1,27 @@
-function dmode(){
-    let body = document.querySelector('body');
-    let nav = document.getElementsByClassName('nav');
+const colors = [
+    [
+    //light group
+        "#21274b",//font
+        "#c9d2df",//bg
+    //dark group
+        "#ded8b4",//font
+        "#131415",//bg
+    //others
+        "#c7ccd1"
+    ],
+];
 
-    body.style.backgroundColor = rgba(24,26,26,1);
-    nav.style.color = white;
-}
-
+//opening nav bar
 
 function nav(){
     let opt = document.getElementById('opt');
     let innav = document.getElementById('nav');
-    let outnav = document.getElementsByClassName('nav');
-    let panel = document.getElementsByClassName('panel_board'); 
 
     function openNav(){
-        outnav.style.backgroundColor = "#0000ff";
-
         opt.style.flex = '1';
-        opt.style.marginRight = '2.5%';
-        opt.style.width == '7%';
+        opt.style.marginRight = '3%';
     
         innav.style.display = "flex";
-
-        //panel.style.filter = "brightness(50%)";
         console.log('open');
     }
 
